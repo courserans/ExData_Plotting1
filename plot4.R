bc2513f396a417043ea7b00fdef485de88a31e97
courserans <coursera.ns@gmail.com>
2015-01-08 23:26:06 +0530

    Project 1 : Plot 4
    Exploratory Data analysis

---
+++ plot4.R
@@ -0,0 +1,26 @@
+## Plot 4 : Project 1 : Exploratory Data Analysis
+
+## 1. Read the source data
+## 2. Use the Transform and subset function to select the valid data set
+## 3. Use the plot and the png function to create a graph and generate a plot4.png
+
+## The assumption is the source data file is in the current working directory
+
+rawdata <- read.csv("household_power_consumption.txt", sep =";",na.strings="?",stringsAsFactors=FALSE)
+rawdata <- transform(rawdata, Date=as.Date(Date,"%d/%m/%Y"))
+data <- subset(rawdata, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+
+png(file = "plot4.png", width=480, height=480 ) 
+
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0)) 
+
+plot(as.POSIXct(paste(data$Date, data$Time)),data$Global_active_power,type ="l",xlab="", ylab = "Global Active Power")
+plot(as.POSIXct(paste(data$Date, data$Time)),data$Voltage,type ="l",xlab="datetime", ylab = "Voltage")
+plot(as.POSIXct(paste(data$Date, data$Time)),data$Sub_metering_1,type ="l",xlab="", ylab = "Energy sub metering")
+lines(as.POSIXct(paste(data$Date, data$Time)),data$Sub_metering_2, col = "red")
+lines(as.POSIXct(paste(data$Date, data$Time)),data$Sub_metering_3, col = "blue")
+legend ("topright", lty=c(1,1,1), 
+        col = c("black", "red", "blue"),
+        legend = c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "))
+plot(as.POSIXct(paste(data$Date, data$Time)),data$Global_reactive_power,type ="l",xlab="datetime", ylab = "Global_reactive_power")
+dev.off()
